dd97b560307e7c1a22a6420d21b9ae9bd0d2631c
Aniket Raj <aniketraj0224@gmail.com>
2024-09-28 01:18:47 +0530

    Add files via upload

---
+++ Python/hello.py
@@ -0,0 +1,17 @@
+# Here is a sample line of code that can be executed in Python:
+
+# print("Hello, World!")
+# You can just as easily store a string as a variable and then print it to stdout:
+
+# my_string = "Hello, World!"
+# print(my_string)
+# The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+# NOTE: Method 1
+if __name__ == '__main__':
+    print("Hello, World!")
+
+#NOTE: Method 2
+if __name__ == '__main__':
+    my_string = ("Hello, World!")
+    print(my_string)
