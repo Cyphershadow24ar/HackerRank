3a664bb23dd37eb4efe883396114f102a8cc6a19
Aniket Raj <aniketraj0224@gmail.com>
2024-10-01 00:12:43 +0530

    Update if-else.py

--- Python/if-else.py
+++ Python/if-else.py
@@ -1,20 +1,21 @@
-Question:
-Given an integer, n, perform the following conditional actions:
+# Question:
+# Given an integer, n, perform the following conditional actions:
 
-If n is odd, print Weird
-If n is even and in the inclusive range of 2 to 5, print Not Weird.
-If n is even and in the inclusive range of 6 to 20, print Weird.
-If n is even and greater than 20, print Not Weird.
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird.
+# If n is even and in the inclusive range of 6 to 20, print Weird.
+# If n is even and greater than 20, print Not Weird.
 
 
-Solution: - 
+# Solution: - 
 
-import math
-import os
-import random
-import re
-import sys
+# import math
+# import os
+# import random
+# import re
+# import sys
 
+# solution:-
 if __name__ == '__main__':
     n = int(input().strip())
 if n % 2 != 0:  # Odd number
