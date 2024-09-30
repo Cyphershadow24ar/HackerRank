e2f4c8159abeae5a4b6c1b1fee429a923e2c30a0
Aniket Raj <aniketraj0224@gmail.com>
2024-10-01 00:10:20 +0530

    Update arithmetic operators.py

--- Python/arithmetic operators.py
+++ Python/arithmetic operators.py
@@ -1,20 +1,20 @@
 # Task
-Question: - 
-The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+# Question: - 
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
 
-1. The first line contains the sum of the two numbers.
-2. The second line contains the difference of the two numbers (first - second).
-3. The third line contains the product of the two numbers.
-Example
-a = 3
-b = 5
+# 1. The first line contains the sum of the two numbers.
+# 2. The second line contains the difference of the two numbers (first - second).
+# 3. The third line contains the product of the two numbers.
+# Example
+# a = 3
+# b = 5
 
-Print the following:
-1. a + b = 8
-2. a-b = -2
-3. a*c = 15
+# Print the following:
+# 1. a + b = 8
+# 2. a-b = -2
+# 3. a*c = 15
 
-Solution: -
+# Solution: -
 
 if __name__ == '__main__':
     a = int(input())
