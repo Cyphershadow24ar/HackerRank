74a83dedd759e96a0808e97ca15a515879a5c222
Aniket Raj <aniketraj0224@gmail.com>
2024-10-01 00:09:27 +0530

    Create loop.py

---
+++ Python/loop.py
@@ -0,0 +1,19 @@
+# Task
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers i< n , print i^2 .
+
+# Example
+
+# The list of non-negative integers that are less than n =3 is [0,1,2]. Print the square of each number on a separate line.
+
+# 0
+# 1
+# 4
+
+# Solution: -
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range (n):
+        print(i**2)
+        
